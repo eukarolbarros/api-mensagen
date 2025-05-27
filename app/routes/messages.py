2f8098526fefabecc5385591e2eba12c9e74307f
Karol barros <130667582+eukarolbarros@users.noreply.github.com>
2025-05-27 22:25:19 +0000

    3.7  a 3.9 - notion vicente

--- app/routes/messages.py
+++ app/routes/messages.py
@@ -4,4 +4,7 @@ messages_bp = Blueprint('messages', __name__) #criou um blueprint
 
 @messages_bp.route("/", methods=["GET"])
 def get_messages(): #retorna lista de mensagens, json
-    return jsonify({"messages": ["Olá, mundo!", "Essa é a segunda mensagem."]})
+    messages = Message.query.order_by(Message.created_at.desc()).all() # busca todas as mensagens da tabela no bd e organiza da mais recente para a mais antiga, o for msg in menssages tranforma cada obj em um dicionario
+    return jsonify([msg.to_dict() for msg in messages])
+
+    
